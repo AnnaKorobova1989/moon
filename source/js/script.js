7c43cd2440a9c3cb1ed106f772153a0be0476cff
Anna <korobovaanna89@gmail.com>
2021-07-25 14:53:42 +0300

    исправление багов в скрипте

--- source/js/script.js
+++ source/js/script.js
@@ -2,52 +2,57 @@
 
 (function () {
   var page = document.querySelector('.page');
+
   if (page.querySelector('.menu')) {
     var menu = page.querySelector('.menu');
-    var menuList = menu.querySelector('.menu__list');
-    var menuToggle = menu.querySelector('.menu__toggle');
-
-    var menuOpen = function () {
-      page.classList.add('page--disabled');
-      menuToggle.classList.add('menu__toggle--open');
-      menu.classList.add('menu--open');
-      menuList.classList.add('menu__list--open');
-      menuList.addEventListener('click', outsideClickHandler);
-      menu.addEventListener('keydown', keyPressHandler);
-    };
-
-    var menuClose = function () {
-      page.classList.remove('page--disabled');
-      menuToggle.classList.remove('menu__toggle--open');
-      menu.classList.remove('menu--open');
-      menuList.classList.remove('menu__list--open');
-      menuList.removeEventListener('click', outsideClickHandler);
-      menu.removeEventListener('keydown', keyPressHandler);
-    };
-
-    var keyPressHandler = function (evt) {
-      if (evt.key === 'Escape') {
-        evt.preventDefault();
-
-        menuClose();
-      }
-    };
-
-    var outsideClickHandler = function () {
-      menuClose();
-    };
-
     menu.classList.remove('menu--no-js');
-    menuList.classList.remove('menu__list--no-js');
-    menuToggle.classList.remove('menu__toggle--no-js');
 
-    menuToggle.addEventListener('click', function () {
-      if (menu.classList.contains('menu--open')) {
+    if ((menu.querySelector('.menu__list')) && (menu.querySelector('.menu__toggle'))) {
+      var menuList = menu.querySelector('.menu__list');
+      var menuToggle = menu.querySelector('.menu__toggle');
+
+      var menuOpen = function () {
+        page.classList.add('page--disabled');
+        menuToggle.classList.add('menu__toggle--open');
+        menu.classList.add('menu--open');
+        menuList.classList.add('menu__list--open');
+        menuList.addEventListener('click', outsideClickHandler);
+        menu.addEventListener('keydown', keyPressHandler);
+      };
+
+      var menuClose = function () {
+        page.classList.remove('page--disabled');
+        menuToggle.classList.remove('menu__toggle--open');
+        menu.classList.remove('menu--open');
+        menuList.classList.remove('menu__list--open');
+        menuList.removeEventListener('click', outsideClickHandler);
+        menu.removeEventListener('keydown', keyPressHandler);
+      };
+
+      var keyPressHandler = function (evt) {
+        if (evt.key === 'Escape') {
+          evt.preventDefault();
+
+          menuClose();
+        }
+      };
+
+      var outsideClickHandler = function () {
         menuClose();
-      } else {
-        menuOpen();
-      }
-    });
+      };
+
+      menuList.classList.remove('menu__list--no-js');
+      menuToggle.classList.remove('menu__toggle--no-js');
+
+      menuToggle.addEventListener('click', function () {
+        if (menu.classList.contains('menu--open')) {
+          menuClose();
+        } else {
+          menuOpen();
+        }
+      });
+
+    }
   }
 })();
 
